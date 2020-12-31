5494e27ecf316ed495f3af6d4594defb18753d41
Daniel Developer <danielfelipedeveloper@gmail.com>
2020-12-31 10:30:43 -0400

    feat: estilizando Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="/teste">
+          <img
+            src="https://avatars0.githubusercontent.com/u/61925435?s=460&u=a9738209b73f083d3c746304e02a663ca4cbe230&v=4"
+            alt="Daniel Felipe"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly sacalable ReactJS & React Native forms!</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
